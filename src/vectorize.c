0101c1d7fae4fca9bca95777326785d33847287d
Kyle Dent <kdent@parc.com>
2014-12-23 08:24:37 -0800

    Fixes memory allocation for word vector.

--- src/vectorize.c
+++ src/vectorize.c
@@ -8,6 +8,13 @@
 #include <vectorize.h>
 #include <vocab_list.h>
 
+/*
+ * Projects words from a list of documents into a vector. The pointer to the
+ * vector should be freed by calling vector_free().
+ *
+ * If successful, returns a pointer to a 2-dimensional array. If memory
+ * cannot be allocated, it returns NULL.
+ */
 int **
 vectorize_doc_list(VocabList *vlist, DocList *dlist)
 {
@@ -15,14 +22,15 @@ vectorize_doc_list(VocabList *vlist, DocList *dlist)
     char *tok;
     int doc_count = 0;
     VocabItem *vocab_item;
-    int **v = (int **)malloc(dlist->cur_size * sizeof(int *));
+    int **v = (int **)malloc(dlist->size * sizeof(int *));
     if (v == NULL) return NULL;
 
     for (doc_features = doc_list_first(dlist); doc_features != NULL;
     doc_features = doc_list_next(dlist))
     {
-        v[doc_count] = (int *)malloc(vlist->item_count * sizeof(int));
-        memset(v[doc_count], 0, vlist->item_count);
+        v[doc_count] = (int *)malloc(vlist->size * sizeof(int));
+        if (v[doc_count] == NULL) return NULL;
+        memset(v[doc_count], 0, vlist->size);
         for (tok = str_list_first(doc_features->token_list); tok != NULL;
         tok = str_list_next(doc_features->token_list))
         {
@@ -33,4 +41,14 @@ vectorize_doc_list(VocabList *vlist, DocList *dlist)
     }
 
     return v;
+}
+
+void
+vector_free(int **v, int nrows)
+{
+    int i;
+    for (i = 0; i < nrows; i++) {
+        free(v[i]);
+    }
+    free(v);
 }
